9786e98ccfbd8584bdad87afda89978d4ddf184b
Kirby Wu <tkirby@gmail.com>
2017-08-21 01:28:16 +0800

    init database

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 /usr/sbin/nginx -c /srv/web/nginx.config
+/srv/web/node_modules/.bin/lsc /srv/web/engine/io/postgresql/init.ls
 /srv/web/node_modules/.bin/lsc server.ls &> /srv/web/static/error.txt
